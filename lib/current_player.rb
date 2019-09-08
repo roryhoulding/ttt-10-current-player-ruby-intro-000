c783b63230750f8ecdcd6956c30909159e43c2c5
Rory H <rohoulding@googlemail.com>
2019-09-08 12:58:51 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,6 +2,10 @@ def turn_count(board)
   board.count { |x| (x != " ") && (x != "") }
 end
 
-def current_player
-
+def current_player(board)
+  if turn_count(board).even?
+    "X"
+  else
+    "O"
+  end
 end
